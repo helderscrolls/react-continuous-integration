e2913ed9c8f42a61daab5a32437d6fb26ae89d69
Helder Nascimento <helder@lifeaz.fr>
2019-12-10 13:14:10 +0100

    Renamed custom URL

--- scripts/deploy_staging_app.sh
+++ scripts/deploy_staging_app.sh
@@ -1,3 +1,3 @@
-echo "{ "alias": "staging-helderscrolls-ci" }" > now-staging-app.json
+echo "{ "alias": "staging-helderscrollsdevops" }" > now-staging-app.json
 now --public --token $NOW_TOKEN
 now alias --token $NOW_TOKEN --local-config now-staging-app.json
